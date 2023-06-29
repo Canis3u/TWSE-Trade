15216bb5b0a5cfe81d688ca6286c8d68cd1b0f72
Canis3u <Canis.Wu@ibm.com>
2023-06-29 13:36:24 +0800

    edit TradeRespViewModel
    edit it for frontend using: change type DateTime to string, and automapper profile also be adjusted for above change

--- TWSE Trade Web API/Profiles/TradeProfile.cs
+++ TWSE Trade Web API/Profiles/TradeProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using System;
 using TWSE_Trade_Web_API.Models;
 using TWSE_Trade_Web_API.ServiceModel;
 using TWSE_Trade_Web_API.Utils;
@@ -11,25 +12,47 @@ namespace TWSE_Trade_Web_API.Profiles
 
         public TradeProfile()
         {
-            CreateMap<TradeQueryViewModel, TradeQueryServiceModel>();
+            CreateMap<TradeQueryViewModel, TradeQueryServiceModel>()
+                .ForMember(
+                    member => member.StartDate,
+                    opt => opt.MapFrom(src => DateTime.ParseExact(src.StartDate, "yyyy-MM-dd", null).ToString("yyyyMMdd"))
+                )
+                .ForMember(
+                    member => member.EndDate,
+                    opt => opt.MapFrom(src => DateTime.ParseExact(src.EndDate, "yyyy-MM-dd", null).ToString("yyyyMMdd"))
+                );
             CreateMap<TradeUpdateViewModel, TradeUpdateServiceModel>()
                 .ForMember(
                     member => member.Status,
                     opt => opt.MapFrom(src => 1)
                 );
             CreateMap<TradeUpdateServiceModel, Trade>();
-
-
             CreateMap<TradeQueryRespServiceModel, TradeQueryRespViewModel>();
-            CreateMap<TradeQueryServiceModel, TradeQueryViewModel>();
+            CreateMap<TradeQueryServiceModel, TradeQueryViewModel>()
+                .ForMember(
+                    member => member.StartDate,
+                    opt => opt.MapFrom(src => DateTime.ParseExact(src.StartDate, "yyyyMMdd", null).ToString("yyyy-MM-dd"))
+                )
+                .ForMember(
+                    member => member.EndDate,
+                    opt => opt.MapFrom(src => DateTime.ParseExact(src.EndDate, "yyyyMMdd", null).ToString("yyyy-MM-dd"))
+                );
             CreateMap<TradeRespServiceModel, TradeRespViewModel>()
                 .ForMember(
-                    member => member.ReturnDate,
-                    opt => opt.MapFrom(src => src.TradeDate.AddDays(src.LendingPeriod))
+                    member => member.StockIdAndName,
+                    opt => opt.MapFrom(src => $"{src.StockId} {src.StockName}")
                 )
                 .ForMember(
                     member => member.Type,
                     opt => opt.MapFrom(src => TransformTypeTools.TransformTypeCode(src.Type))
+                )
+                .ForMember(
+                    member => member.TradeDate,
+                    opt => opt.MapFrom(src => src.TradeDate.ToString("yyyy-MM-dd"))
+                )
+                .ForMember(
+                    member => member.ReturnDate,
+                    opt => opt.MapFrom(src => src.TradeDate.AddDays(src.LendingPeriod).ToString("yyyy-MM-dd"))
                 );
         }
     }
--- TWSE Trade Web API/ViewModel/TradeRespViewModel.cs
+++ TWSE Trade Web API/ViewModel/TradeRespViewModel.cs
@@ -5,14 +5,13 @@ namespace TWSE_Trade_Web_API.ViewModel
     public class TradeRespViewModel
     {
         public int Id { get; set; }
-        public string StockId { get; set; }
-        public string StockName { get; set; }
-        public DateTime TradeDate { get; set; }
+        public string StockIdAndName { get; set; }
+        public string TradeDate { get; set; }
         public string Type { get; set; }
         public int Volume { get; set; }
         public float Fee { get; set; }
         public float ClosingPrice { get; set; }
         public int LendingPeriod { get; set; }
-        public DateTime ReturnDate { get; set; }
+        public string ReturnDate { get; set; }
     }
 }
