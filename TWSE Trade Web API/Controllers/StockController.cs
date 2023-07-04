6efd1632ef50f0c23cbfa7dccb180e52342e6a61
Canis3u <Canis.Wu@ibm.com>
2023-07-04 10:08:45 +0800

    init line shape

---
+++ TWSE Trade Web API/Controllers/StockController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TWSE_Trade_Web_API.ViewModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TWSE_Trade_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        // GET: api/<StockController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StockController>/5
+        [HttpGet("{id}")]
+        public StockRespViewModel GetStockInformationByStockIdAsync(string stockId)
+        {
+            var respViewModel = new StockRespViewModel();
+            return respViewModel;
+        }
+    }
+}
---
+++ TWSE Trade Web API/ViewModel/StockRespViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TWSE_Trade_Web_API.ViewModel
+{
+    public class StockRespViewModel
+    {
+        public string StockId { get; set; }
+        public string Name { get; set; }
+    }
+}
